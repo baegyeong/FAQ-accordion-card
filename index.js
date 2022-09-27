b98ec0dbddea9012f6f52ba213c0ed83e34b425e
baegyeong <beth0929@naver.com>
2022-09-27 21:32:32 +0900

    feat: faq DOM 구성

---
+++ index.js
@@ -0,0 +1,48 @@
+const faqList = [
+  {
+    title: "How many team members can I invite?",
+    contents:
+      "You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.",
+  },
+  {
+    title: "What is the maximum file upload size?",
+    contents:
+      " No more than 2GB. All files in your account must fit your allotted storage space.",
+  },
+  {
+    title: "How do I reset my password?",
+    contents:
+      "Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.",
+  },
+  {
+    title: "Can I cancel my subscription?",
+    contents:
+      "Yes! Send us a message and we’ll process your request no questions asked.",
+  },
+  {
+    title: "Do you provide additional support?",
+    contents:
+      "Chat and email support is available 24/7. Phone lines are open during normal business hours.",
+  },
+];
+const faqContainer = document.querySelector(".faq-container");
+
+faqList.forEach((faq) => {
+  const faqElement = document.createElement("div");
+  const headerElement = document.createElement("div");
+  const titleElement = document.createElement("span");
+  const arrowImg = document.createElement("img");
+  const contentElement = document.createElement("div");
+
+  titleElement.innerText = faq.title;
+  arrowImg.src = "./images/icon-arrow-down.svg";
+  contentElement.innerText = faq.contents;
+
+  headerElement.appendChild(titleElement);
+  headerElement.appendChild(arrowImg);
+
+  faqElement.appendChild(headerElement);
+  faqElement.appendChild(contentElement);
+
+  faqContainer.appendChild(faqElement);
+});
